654f6bd8c3df9760c91456ab37ebe2d8b64e05f9
useradmin <amutawa@hotmail.com>
2024-11-18 09:51:53 +0300

    controllers

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,63 +1,84 @@
 const express = require('express');
-const router = express.Router();
+const verifyToken = require('../middleware/verify-token');
 const Course = require('../models/course');
+const router = express.Router();
 
-// get all courses
+// ======= Public Routes ===========
+
+// INDEX - GET ALL COURSES
 router.get('/', async (req, res) => {
     try {
-        const courses = await Course.find({});
-        res.json({ courses });
+        const courses = await Course.find({}).sort({ createdAt: 'desc' });
+        res.status(200).json(courses);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json(error);
     }
 });
 
-// post new course
-router.post('/', async (req, res) => {
-    try {
-        if (req.user.role !== 'instructor') {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-        req.body.instructor = req.user._id;
-        const course = new Course(req.body);
-        await course.save();
-        res.status(201).json({ course });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
-
-// get a course by id
+// SHOW - GET ONE COURSE BY ID
 router.get('/:id', async (req, res) => {
     try {
         const course = await Course.findById(req.params.id);
-        if (course) {
-            res.json({ course });
-        } else {
-            res.status(404).json({ error: 'Course not found' });
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' });
         }
+        res.status(200).json(course);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json(error);
     }
 });
 
-// update a course by id
+// ======= Protected Routes ==========
+router.use(verifyToken);
+
+// CREATE COURSE
+router.post('/', async (req, res) => {
+    try {
+        if (req.user.role !== 'instructor') {
+            return res.status(403).json({ error: 'Unauthorized' });
+        }
+        req.body.instructor = req.user._id;
+        const course = await Course.create(req.body);
+        course._doc.instructor = req.user;
+        res.status(201).json(course);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// UPDATE COURSE
 router.put('/:id', async (req, res) => {
     try {
-        if (req.body.instructor !== req.user._id) {
-            return res.status(401).json({ error: 'Unauthorized' });
+        const course = await Course.findById(req.params.id);
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' });
         }
-        const course = await Course.findByIdAndUpdate
-        (req.params.id, req.body, { new: true });
-        if (course) {
-            res.json({ course });
-        } else {
-            res.status(404).json({ error: 'Course not found' });
+        if (course.instructor.toString() !== req.user._id) {
+            return res.status(403).json({ error: 'Unauthorized' });
         }
+        const updatedCourse = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        updatedCourse._doc.instructor = req.user;
+        res.status(200).json(updatedCourse);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json(error);
     }
 });
 
+// DELETE COURSE
+router.delete('/:id', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id);
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' });
+        }
+        if (course.instructor.toString() !== req.user._id) {
+            return res.status(403).json({ error: 'Unauthorized' });
+        }
+        await course.deleteOne();
+        res.status(200).json({ message: 'Course deleted successfully' });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
